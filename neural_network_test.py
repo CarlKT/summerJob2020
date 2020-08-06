39bc9a6e0db8a0654c80111af06216406fcc86e5
Carl Kristof Tessier <carlkristoftessier@gmail.com>
2020-08-06 15:50:57 -0400

    Update neural_network_test.py

--- neural_network_test.py
+++ neural_network_test.py
@@ -3,34 +3,47 @@ import tensorflow as tf
 from sklearn.datasets import load_iris
 import pandas as pd
 import numpy as np
-from tf.keras.layers import Dense, Input
-from tf.keras import Model
-
-features, labels = load_iris(return_X_y=True)
-features_df = pd.DataFrame(features)
-
-predictors = {
-    column_name: list(column_data)
-    for column_name, column_data in features_df.iteritems()
-}
-print(predictors[0])
+from tensorflow.keras.layers import Dense, Input
+from tensorflow.keras import Model
 
 
-def create_class_model(num_inputs, num_outputs):
+def create_class_model(num_inputs, num_outputs, hidden_shape, summary=True):
+    """Creates a classification model with any number of inputs, outputs. Additionaly,
+        hidden_shape configures hidden layer architecture, with each element of
+        hidden_layer represents the number of nodes in that layer"""
 
     input_tensor = Input(shape=(num_inputs, ))
-    output_tensor = Dense(num_outputs, activation='softmax')(input_tensor)
+    tensors = [input_tensor]
+
+    #Create hidden layers
+    for i in range(len(hidden_shape)):
+        num_nodes = hidden_shape[i]
+        hidden_tensor_0 = tensors[i]
+        tensor_name = 'hidden_layer_%s' % (i + 1)
+        hidden_tensor_1 = Dense(num_nodes, activation='relu',
+                                name=tensor_name)(hidden_tensor_0)
+        tensors.append(hidden_tensor_1)
+
+    output_tensor = Dense(num_outputs, activation='softmax',
+                          name='output')(tensors[-1])
 
     model = Model(input_tensor, output_tensor)
     model.compile(optimizer='adam',
                   loss='mean_absolute_error',
                   metrics=['accuracy'])
-
+    if summary:
+        print(model.summary())
     return model
 
 
-num_inputs = range(predictors.keys())
-num_outputs = np.unique(labels)
-model = create_class_model(num_inputs, num_outputs)
+#def train_model(predictors, labels, model):
+features, labels = load_iris(return_X_y=True)
+df = pd.DataFrame(features)
+df['labels'] = labels
 
-print(model.summarize())
+num_inputs = features.shape[1]
+num_outputs = len(np.unique(labels))
+hidden_shape = [100, 100]
+model = create_class_model(num_inputs, num_outputs, hidden_shape, summary=True)
+
+#model.fit(df[[0, 1, 2, 3]], df['labels'], epochs=20, validation_split=0.2)
